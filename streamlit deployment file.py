95aae8b89b0d0d1f767f769c96e2df92c4caaacb
Syed Atif Ali <157229119+syedatifkashifali@users.noreply.github.com>
2024-02-11 06:38:31 -0800

    Create streamlit deployment file.py

---
+++ streamlit deployment file.py
@@ -0,0 +1,10 @@
+#when running python compiler the streamlit library will not work.
+import streamlit as st #converting the name streamlit to short variable "st"
+
+#accessing methods from the streamlit library
+st.title("Hello, World!")
+st.write("Hi, it's me")
+st.title("My Name : Syed Atif Ali")
+st.write("I am student at banoqabil")
+st.write("Class: CIT with Python")
+print("hello, worlds")#this python command will not work since we run on streamlit, only streamlit method will work here.
